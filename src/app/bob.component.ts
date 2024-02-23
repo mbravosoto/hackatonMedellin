367a361a30c4db850110172424a9ddb885de028a
Manuela Bravo Soto <m-bravo@javeriana.edu.co>
2024-02-23 17:35:20 -0500

    feat: :sparkles: Modify initial page

---
+++ src/app/bob.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    standalone: true,
+    selector: 'bob-test',
+    template: `
+    jijijija
+    `
+})
+
+export class BobTest {
+}
