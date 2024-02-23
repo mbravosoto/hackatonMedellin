5745262694088bd52ecbe7bb7da701b924426a65
mestupinanm <111020866+mestupinanm@users.noreply.github.com>
2024-02-23 18:02:12 -0500

    cambios con fondito

--- src/app/bob.component.ts
+++ src/app/bob.component.ts
@@ -2,11 +2,13 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
     standalone: true,
+    // eslint-disable-next-line @angular-eslint/component-selector
     selector: 'bob-test',
     template: `
     jijijija
     `
 })
 
+// eslint-disable-next-line @angular-eslint/component-class-suffix
 export class BobTest {
 }
