50f9e4873e5dd3ec666b619de96c9399577a97ec
mestupinanm <111020866+mestupinanm@users.noreply.github.com>
2024-02-23 22:19:38 -0500

    funciona!

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,21 @@ import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { BobTest } from './bob.component';
+import { FormsModule } from '@angular/forms'; // Import FormsModule
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule, BobTest],
+  imports: [NxWelcomeComponent, RouterModule, BobTest, FormsModule], // Include FormsModule here
   selector: 'dapp-angular-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
+  styleUrls: ['./app.component.scss'], // Corrected from 'styleUrl' to 'styleUrls'
 })
 export class AppComponent {
   title = 'dapp-angular';
+  sliderValue = 20; 
+  updateValue() {
+    console.log(this.sliderValue);
+  }
 }
+
+
