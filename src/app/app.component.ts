0bf854eab618e0391c655ed9083d30059da338e7
mestupinanm <111020866+mestupinanm@users.noreply.github.com>
2024-02-23 23:02:00 -0500

    cambios pequeñitos pero potentes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,21 +2,37 @@ import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { BobTest } from './bob.component';
-import { FormsModule } from '@angular/forms'; // Import FormsModule
+import { FormsModule } from '@angular/forms'; 
+import { CommonModule } from '@angular/common'; 
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule, BobTest, FormsModule], // Include FormsModule here
+  imports: [CommonModule, NxWelcomeComponent, RouterModule, BobTest, FormsModule], // Include FormsModule here
   selector: 'dapp-angular-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'], // Corrected from 'styleUrl' to 'styleUrls'
+  styleUrls: ['./app.component.scss'], 
 })
+
 export class AppComponent {
   title = 'dapp-angular';
-  sliderValue = 18; 
+  sliderValue = 18;
+  backgroundImage = '/assets/Verde.png'; 
+
+  constructor() {
+    this.updateBackgroundImage(); 
+  }
+
   updateValue() {
-    console.log(this.sliderValue);
+    this.updateBackgroundImage(); 
+  }
+
+  updateBackgroundImage() {
+    if (this.sliderValue <= 7) {
+      this.backgroundImage = '/assets/Azul.png'; 
+    } else if (this.sliderValue > 7 && this.sliderValue <= 21) {
+      this.backgroundImage = '/assets/Verde.png'; 
+    } else if (this.sliderValue > 21) {
+      this.backgroundImage = '/assets/Rojo.png'; 
+    }
   }
 }
-
-
